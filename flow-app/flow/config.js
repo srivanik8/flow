513faaefaf97aa4825fcdda24cd454eff00600cc
sri <97357548+srivanik8@users.noreply.github.com>
2023-09-05 13:45:33 +0000

    flow

---
+++ flow-app/flow/config.js
@@ -0,0 +1,13 @@
+import { config } from "@onflow/fcl";
+
+config({
+  "accessNode.api": "https://rest-testnet.onflow.org", 
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn" 
+})
+
+config({
+    "accessNode.api": "https://rest-testnet.onflow.org", 
+    "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",         
+    "0xProfile": "0xba1132bc08f82fe2"
+  })
+  
